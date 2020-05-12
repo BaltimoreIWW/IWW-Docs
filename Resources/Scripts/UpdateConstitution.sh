5be0820cf8add0553a180f511647e6f5837ed7cf
squarepupil <pglaser52alt@gmail.com>
2020-05-12 05:49:21 +0200

    Adding Constitution Updating script

--- Resources/Scripts/UpdateConstitution.sh
+++ Resources/Scripts/UpdateConstitution.sh
@@ -5,3 +5,8 @@
 # edit the favicon.
 
 git pull
+mdmerge /var/www/IWW-Docs/Resources/Scripts/ConstitutionIndex.md -o /var/www/IWW-Docs/Constitution/fulltext.md
+grip --export /var/www/IWW-Docs/Constitution/fulltext.md /var/www/IWW-Docs/Constitution/index.html
+sed -i '/<head>/,/<\/head>/d' /var/www/IWW-Docs/Constitution/index.html
+sed -i '/fulltext.md/d' /var/www/IWW-Docs/Constitution/index.html
+sed -i '2a\<head>\n<meta charset="utf-8" />\n<title>IWW NARA Constitution</title>\n<link rel="stylesheet" type="text/css" href="../Resources/Styles/IWW-Docs.css">\n<link rel="shortcut icon" type="image/ico" href="../Resources/Images/favicon.ico"/>\n</head>' /var/www/IWW-Docs/Constitution/index.html
