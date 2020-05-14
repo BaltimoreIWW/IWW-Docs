40e4cd0d7db4d27c134cc886b3f12137fc14b6e9
squarepupil <pglaser52alt@gmail.com>
2020-05-14 02:47:36 +0200

    Update

--- Resources/Scripts/UpdateConstitution.sh
+++ Resources/Scripts/UpdateConstitution.sh
@@ -3,10 +3,12 @@
 # constitution from github, merges them with mdmerge, converts them to html
 # with grip, and edits the html to include custom css, change the title, and
 # edit the favicon.
+cd /var/www/IWW-Docs/Resources/Scripts
+
 
 git pull
-mdmerge /var/www/IWW-Docs/Resources/Scripts/ConstitutionIndex.md -o /var/www/IWW-Docs/Constitution/fulltext.md
-grip --export /var/www/IWW-Docs/Constitution/fulltext.md /var/www/IWW-Docs/Constitution/index.html
-sed -i '/<head>/,/<\/head>/d' /var/www/IWW-Docs/Constitution/index.html
-sed -i '/fulltext.md/d' /var/www/IWW-Docs/Constitution/index.html
-sed -i '2a\<head>\n<meta charset="utf-8" />\n<title>IWW NARA Constitution</title>\n<link rel="stylesheet" type="text/css" href="../Resources/Styles/IWW-Docs.css">\n<link rel="shortcut icon" type="image/ico" href="../Resources/Images/favicon.ico"/>\n</head>' /var/www/IWW-Docs/Constitution/index.html
+mdmerge ConstitutionIndex.md -o ../../NARA/Constitution/fulltext.md
+grip --export ../../NARA/Constitution/fulltext.md ../../NARA/Constitution/index.html
+sed -i '/<head>/,/<\/head>/d' ../../NARA/Constitution/index.html
+sed -i '/fulltext.md/d' ../../NARA/Constitution/index.html
+sed -i '2a\<head>\n<meta charset="utf-8" />\n<title>IWW NARA Constitution</title>\n<link rel="stylesheet" type="text/css" href="../../Resources/Styles/IWW-Docs.css">\n<link rel="shortcut icon" type="image/ico" href="../../Resources/Images/favicon.ico"/>\n</head>' ../../NARA/Constitution/index.html
