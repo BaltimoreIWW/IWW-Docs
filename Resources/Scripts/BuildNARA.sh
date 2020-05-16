a55dda60e9d9b595e3616cd3e612e26e1d07bcca
squarepupil <pglaser52alt@gmail.com>
2020-05-16 22:43:44 +0200

    Adding Build NARA script

---
+++ Resources/Scripts/BuildNARA.sh
@@ -0,0 +1,3 @@
+cd ../../NARA/Constitution
+cat Title.asc Preamble.asc ArticleI.asc ArticleII.asc ArticleIII.asc ArticleIV.asc ArticleV.asc ArticleVI.asc ArticleVII.asc ArticleVIII.asc ArticleIX.asc ArticleX.asc ArticleXI.asc ArticleXII.asc ArticleXIII.asc ArticleXIV.asc > index.asc
+asciidoctor -d book index.asc
