974a663ee2048227ba973e8a7e6466319eff5b7e
squarepupil <pglaser52alt@gmail.com>
2020-05-16 21:50:02 -0400

    Modified HTML to include Title and Favicon

--- Resources/Scripts/UpdateBaltimoreGMBMPP.sh
+++ Resources/Scripts/UpdateBaltimoreGMBMPP.sh
@@ -2,3 +2,5 @@ cd ../../BaltimoreGMB/MPP
 git pull
 cat Title.asc ITPolicies.asc > index.asc
 asciidoctor -d book index.asc
+sed -i '/<Title>/,/<\/Title>/d' ../../BaltimoreGMB/MPP/index.html
+sed -i '7a\<title>Baltimore IWW GMB Manual of Policies and Procedures</title>\n<link rel="shortcut icon" type="image/ico" href="../../Resources/Images/favicon.ico"/>' ../../BaltimoreGMB/MPP/index.html
