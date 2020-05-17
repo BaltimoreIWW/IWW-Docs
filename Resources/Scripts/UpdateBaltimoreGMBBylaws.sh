8fe026dd32221af5d677054b358c26367477654d
squarepupil <pglaser52alt@gmail.com>
2020-05-16 21:31:43 -0400

    Changed BaltimoreGMB Docs to asc format

--- Resources/Scripts/UpdateBaltimoreGMBBylaws.sh
+++ Resources/Scripts/UpdateBaltimoreGMBBylaws.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
-# This script pulls down the latest versions of each section of the
-# Baltimore GMB Bylaws from github, merges them with mdmerge, converts them to html
-# with grip, and edits the html to include custom css, change the title, and
-# edit the favicon.
-cd /var/www/IWW-Docs/Resources/Scripts
-
+cd ../../BaltimoreGMB/Bylaws
 git pull
-mdmerge BaltimoreGMBBylawsIndex.md -o ../../BaltimoreGMB/Bylaws/fulltext.md
-grip --export ../../BaltimoreGMB/Bylaws/fulltext.md ../../BaltimoreGMB/Bylaws/index.html
-sed -i '/<head>/,/<\/head>/d' ../../BaltimoreGMB/Bylaws/index.html
-sed -i '/fulltext.md/d' ../../BaltimoreGMB/Bylaws/index.html
-sed -i '2a\<head>\n<meta charset="utf-8" />\n<title>Baltimore IWW GMB Bylaws</title>\n<link rel="stylesheet" type="text/css" href="../../Resources/Styles/IWW-Docs.css">\n<link rel="shortcut icon" type="image/ico" href="../../Resources/Images/favicon.ico"/>\n</head>' ../../BaltimoreGMB/Bylaws/index.html
+cat Title.asc ArticleI.asc ArticleII.asc ArticleIII.asc ArticleIV.asc ArticleV.asc ArticleVI.asc ArticleVII.asc ArticleVIII.asc ArticleIX.asc ArticleX.asc > index.asc
+asciidoctor -d book index.asc
--- Resources/Scripts/UpdateBaltimoreGMBMPP.sh
+++ Resources/Scripts/UpdateBaltimoreGMBMPP.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
-# This script pulls down the latest versions of each section of the
-# Baltimore GMB MPP from github, merges them with mdmerge, converts them to html
-# with grip, and edits the html to include custom css, change the title, and
-# edit the favicon.
-cd /var/www/IWW-Docs/Resources/Scripts
-
+cd ../../BaltimoreGMB/MPP
 git pull
-mdmerge BaltimoreGMBMPPIndex.md -o ../../BaltimoreGMB/MPP/fulltext.md
-grip --export ../../BaltimoreGMB/MPP/fulltext.md ../../BaltimoreGMB/MPP/index.html
-sed -i '/<head>/,/<\/head>/d' ../../BaltimoreGMB/MPP/index.html
-sed -i '/fulltext.md/d' ../../BaltimoreGMB/MPP/index.html
-sed -i '2a\<head>\n<meta charset="utf-8" />\n<title>Baltimore IWW GMB Manual of Policies and Procedures</title>\n<link rel="stylesheet" type="text/css" href="../../Resources/Styles/IWW-Docs.css">\n<link rel="shortcut icon" type="image/ico" href="../../Resources/Images/favicon.ico"/>\n</head>' ../../BaltimoreGMB/MPP/index.html
+cat Title.asc ITPolicies.asc > index.asc
+asciidoctor -d book index.asc
