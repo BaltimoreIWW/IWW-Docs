f93528c30a7b1690dcc1af3a7c853baa492e22dc
squarepupil <pglaser52alt@gmail.com>
2020-05-16 22:07:28 -0400

    Update UpdateNARAConstitution.sh

--- Resources/Scripts/UpdateNARAConstitution.sh
+++ Resources/Scripts/UpdateNARAConstitution.sh
@@ -1,6 +1,6 @@
 cd ../../NARA/Constitution
 git pull
 cat Title.asc Preamble.asc ArticleI.asc ArticleII.asc ArticleIII.asc ArticleIV.asc ArticleV.asc ArticleVI.asc ArticleVII.asc ArticleVIII.asc ArticleIX.asc ArticleX.asc ArticleXI.asc ArticleXII.asc ArticleXIII.asc ArticleXIV.asc > index.asc
-asciidoctor -d book index.asc
+asciidoctor -d book -a toc index.asc
 sed -i '/<Title>/,/<\/Title>/d' ../../NARA/Constitution/index.html
 sed -i '7a\<title>IWW Constitution & Preamble</title>\n<link rel="shortcut icon" type="image/ico" href="../../Resources/Images/favicon.ico"/>' ../../NARA/Constitution/index.html
