1f23dfec8c33fb490774bcc304fc01de9ea76c3b
squarepupil <pglaser52alt@gmail.com>
2020-05-16 21:47:09 -0400

    Update UpdateBaltimoreGMBBylaws.sh

--- Resources/Scripts/UpdateBaltimoreGMBBylaws.sh
+++ Resources/Scripts/UpdateBaltimoreGMBBylaws.sh
@@ -3,4 +3,4 @@ git pull
 cat Title.asc ArticleI.asc ArticleII.asc ArticleIII.asc ArticleIV.asc ArticleV.asc ArticleVI.asc ArticleVII.asc ArticleVIII.asc ArticleIX.asc ArticleX.asc > index.asc
 asciidoctor -d book index.asc
 sed -i '/<Title>/,/<\/Title>/d' ../../BaltimoreGMB/Bylaws/index.html
-sed -i '7a\<title>Baltimore IWW GMB Bylaws</title>\n<link rel="shortcut icon" type="image/ico" href="../../Resources/Images/favicon.ico"/> ../../BaltimoreGMB/Bylaws/index.html
+sed -i '7a\<title>Baltimore IWW GMB Bylaws</title>\n<link rel="shortcut icon" type="image/ico" href="../../Resources/Images/favicon.ico"/>' ../../BaltimoreGMB/Bylaws/index.html
