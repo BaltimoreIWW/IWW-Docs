026eb10281f903d0babdadfcac08fbd51101546c
BaltimoreIWW <65121640+BaltimoreIWW@users.noreply.github.com>
2020-05-23 18:03:50 -0400

    Update UpdateBaltimoreGMBBylaws.sh

--- Resources/Scripts/UpdateBaltimoreGMBBylaws.sh
+++ Resources/Scripts/UpdateBaltimoreGMBBylaws.sh
@@ -2,5 +2,5 @@ cd /var/www/IWW-Docs/BaltimoreGMB/Bylaws
 git pull
 cat Title.asc ArticleI.asc ArticleII.asc ArticleIII.asc ArticleIV.asc ArticleV.asc ArticleVI.asc ArticleVII.asc ArticleVIII.asc ArticleIX.asc ArticleX.asc > index.asc
 asciidoctor -d book index.asc
-asciidoctor-pdf -a pdf-theme=../Resources/Styles/IWWDocs-theme.yml Bylaws/index.asc
-mv Bylaws/index.pdf Bylaws/BaltimoreGMBBylaws.pdf
+asciidoctor-pdf -a pdf-theme=../../Resources/Styles/IWWDocs-theme.yml index.asc
+mv index.pdf BaltimoreGMBBylaws.pdf
