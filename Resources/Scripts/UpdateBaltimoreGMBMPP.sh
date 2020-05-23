1c840c44614fe9570d7798a376bb3dfba7321de9
BaltimoreIWW <65121640+BaltimoreIWW@users.noreply.github.com>
2020-05-23 18:27:15 -0400

    Update UpdateBaltimoreGMBMPP.sh

--- Resources/Scripts/UpdateBaltimoreGMBMPP.sh
+++ Resources/Scripts/UpdateBaltimoreGMBMPP.sh
@@ -2,6 +2,6 @@ cd /var/www/IWW-Docs/BaltimoreGMB/MPP
 git pull
 cat Title.asc ITPolicies.asc > index.asc
 asciidoctor -d book index.asc
-sed -i '7a\<link rel="shortcut icon" type="image/ico" href="../../Resources/Images/favicon.ico"/>' ../../BaltimoreGMB/MPP/index.html
+sed -i '7a\<link rel="shortcut icon" type="image/ico" href="../../Resources/Images/favicon.ico"/>' index.html
 asciidoctor-pdf index.asc
 mv index.pdf BaltimoreGMBMPP.pdf
