fd94ead0a6561ffac9ae48fe3ef8eca0c8272787
BaltimoreIWW <65121640+BaltimoreIWW@users.noreply.github.com>
2020-05-23 18:50:09 -0400

    Update UpdateBaltimoreGMBBylaws.sh

--- Resources/Scripts/UpdateBaltimoreGMBBylaws.sh
+++ Resources/Scripts/UpdateBaltimoreGMBBylaws.sh
@@ -3,5 +3,5 @@ git pull
 cat Title.asc ArticleI.asc ArticleII.asc ArticleIII.asc ArticleIV.asc ArticleV.asc ArticleVI.asc ArticleVII.asc ArticleVIII.asc ArticleIX.asc ArticleX.asc > index.asc
 asciidoctor -d book index.asc
 sed -i '7a\<link rel="shortcut icon" type="image/ico" href="../../Resources/Images/favicon.ico"/>' index.html
-asciidoctor-pdf -a pdf-theme=../../Resources/Styles/IWWDocs-theme.yml index.asc
+asciidoctor-pdf -a pdf-theme=../../Resources/Styles/BaltimoreGMB-theme.yml index.asc
 mv index.pdf BaltimoreGMBBylaws.pdf
