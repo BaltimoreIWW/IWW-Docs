36cfc4a23b2a5f7db62b1d38a419f2be168165c6
BaltimoreIWW <65121640+BaltimoreIWW@users.noreply.github.com>
2021-03-14 16:27:51 -0400

    Update UpdateBaltimoreGMBMPP.sh

--- Resources/Scripts/UpdateBaltimoreGMBMPP.sh
+++ Resources/Scripts/UpdateBaltimoreGMBMPP.sh
@@ -1,6 +1,6 @@
 cd /var/www/IWW-Docs/BaltimoreGMB/MPP
 git pull
-cat Title.asc ITPolicies.asc CampaignsCommitteeBylaws.asc > index.asc
+cat Title.asc ITPolicies.asc DigitalCommunicationsPolicy.asc CampaignsCommitteeBylaws.asc > index.asc
 asciidoctor -d book index.asc
 sed -i '7a\<link rel="shortcut icon" type="image/ico" href="../../Resources/Images/favicon.ico"/>\n<a href="BaltimoreGMBMPP.pdf"><div class="fab"><img src="../../Resources/Images/pdf-button.png" alt="Print-Friendly PDF"></div></a>\n<link rel="stylesheet" type="text/css" href="../../Resources/Styles/IWW-Docs.css">' index.html
 asciidoctor-pdf -a pdf-theme=../../Resources/Styles/BaltimoreGMB-theme.yml index.asc
